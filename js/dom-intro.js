1c4114fae6690f2949905b929f8a8e2fd92acedb
unknown <nduflot@dawan.fr>
2026-03-03 14:38:14 +0100

    intro js partie 7 DOM et événements

--- js/dom-intro.js
+++ js/dom-intro.js
@@ -11,4 +11,63 @@ window.addEventListener('DOMContentLoaded', function(){
         console.log(titrePrincipal);
         console.log(event);
     });
+
+    /*
+    sélecteurs CSS résumés
+    p                   => sélecteur balise, on utilise le nom de la balise, ici <p></p>
+    .titreDeChapitre    => un élément ayant au moins la classe 'titreDeChapitre'
+    #titre-principal    => un élément ayant l'id 'titre-principal'
+
+    les sélecteurs CSS permettent d'utiliser deux méthodes de récupération d'élément
+    querySelector(sélecteur css) => récupère la première occurence correspondant au sélecteur
+    querySelectorAll(sélecteur css) => récupère une collection déléments correspondants au sélecteur
+    */
+
+    console.log(document.querySelector('p'));
+    console.log(document.querySelector('header > div.container'));
+    console.log(document.querySelectorAll('div.container'));
+    console.log(document.querySelectorAll('p'));
+
+    /* Repérer le clic sur le bouton */
+    const buttonTest = document.querySelector('.test');
+    buttonTest.addEventListener('click', function(event){
+        event.stopPropagation();
+        console.log('coucou bouton');
+    });
+
+    buttonTest.parentElement.addEventListener('click', function(event){
+        event.stopPropagation();
+        console.log('coucou paragraphe');
+    });
+
+    /*
+    il est possible d'éviter de sélection des élément qui correspondent à minima au sélecteur du querySelector ou querySelectorAll
+    p => tous les paragraphes
+    p:not(.special) => tout les paragraphes qui ne contiennent la classe 'special'
+    */
+
+    document.querySelectorAll('p:not(.special)').forEach(function(para){
+        /*console.log(para);*/
+        para.addEventListener('click', function(){
+            this.classList.toggle('fondGris');
+            document.getElementById('showP').innerHTML = this.innerText;
+        });
+    });
+
+    /*
+    repérer quand le pointeur de la souris arrive et ressort du "périmètre" d'une image pour déclencher deux actions différentes
+    */
+
+    const imgOver = document.getElementById('img-over');
+
+    imgOver.addEventListener('mouseenter', function(){
+        this.setAttribute('src', './images/pip-boy-thumb-up.png');
+        this.setAttribute('data-toto', './images/pip-boy-thumb-up.png');
+    });
+    
+    imgOver.addEventListener('mouseleave', function(){
+        this.setAttribute('src', './images/pip-boy-thumb-down.png');        
+        this.removeAttribute('data-toto');
+    });
+
 });

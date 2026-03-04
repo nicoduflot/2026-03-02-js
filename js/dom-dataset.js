a61aa7b059fc001829c242ddbaeac616e9170a62
unknown <nduflot@dawan.fr>
2026-03-04 12:35:25 +0100

    intro js partie 14 dataset coordonnées

--- js/dom-dataset.js
+++ js/dom-dataset.js
@@ -117,5 +117,75 @@ loaded(function(){
     Le bouton 1 doit action le menu 1 et le bouton 2 le menu 2
     */
 
+    /*
+    Menu glissant
+    Il s'agit simplement d'ajouter une classe à un élément qui est situé hors affichage (en dehors du viewport)
+
+    On repère le clic sur les boutons avec la classe toggle-menu
+    Le bouton cliqué nous donne le menu ciblé grâce au dataset target
+    on ajoute à la cible la classe qui déclenche l'action du menu
+    quand on clique à nouveau sur le bouton on enlève la classe et le menu sort de l'écran
+    */
+
+    q('button.toggle-menu').map(function(button){
+        button.addEventListener('click', function(){
+            q(this.dataset.target).classList.toggle('toggle-element');
+        });
+    });
+
+    /*
+    On ajoute un bouton pour fermer le menu dans le menu
+
+    On clic sur ce bouton close contenu dans une nav ayant la classe slide-menu
+    on reprère l'élément parent du bouton
+    on remove la classe toggle-element du parent
+    */
+
+    q('.slide-menu .btn-close').map(function(button){
+        button.addEventListener('click', function(){
+            this.parentElement.classList.remove('toggle-element');
+        });
+    });
+
+    /* 
+    Interactivité sur un tableau
+    parcourir toutes les td du tableau id coord, abonner chaque td aux événements mouseenter et mouseleave
+        - mouseenter : on récupère les dataset de la td column et line
+        on applique à chaque td ayant la même colonne un background color rgba(255,0,0,0.5)
+        on applique à la tr parent le même background color
+
+        - mouseleave :  on récupère les dataset de la td column et line
+        on retire le background color des td avec le meme dataset column
+        on retire le background de la tr parente
+    */
+
+    q('#coord td').map(function(td){
+        td.addEventListener('mouseenter', function(){
+            const column = this.dataset.column;
+            const line = this.dataset.line;
+            q('#xy').innerHTML = `x : ${column} - y : ${line}`;
+            q('#xValue').value = column;
+            q('#yValue').value = line;
+            q(`#coord td[data-column="${column}"]`).map(function(tdy){
+                tdy.style.setProperty('background-color', 'rgba(255, 0, 0, 0.5');
+            });
+            q(`#coord td[data-line="${line}"]`).map(function(tdx){
+                tdx.style.setProperty('background-color', 'rgba(255, 0, 0, 0.5');
+            });
+        });
+        td.addEventListener('mouseleave', function(){
+            const column = this.dataset.column;
+            const line = this.dataset.line;
+            q('#xy').innerHTML = `x : &nbsp; - y : &nbsp;`;
+            q('#xValue').value = 0;
+            q('#yValue').value = 0;
+            q(`#coord td[data-column="${column}"]`).map(function(tdy){
+                tdy.style.removeProperty('background-color');
+            });
+            q(`#coord td[data-line="${line}"]`).map(function(tdx){
+                tdx.style.removeProperty('background-color');
+            });
+        });
+    });
 
 });

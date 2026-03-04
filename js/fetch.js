4966c70d4b768159744a56c5aca5013f35d84538
unknown <nduflot@dawan.fr>
2026-03-04 17:06:19 +0100

    intro js partie 18 fetch et manipuler les résultats json

--- js/fetch.js
+++ js/fetch.js
@@ -1,3 +1,63 @@
+function jsonToTable(data, searchTerm = ''){
+    if(!(data.length > 1)){
+        data = [data];
+    }
+    const table = cE('table');
+    table.classList.add('table', 'table-striped', 'table-bordered');
+    const thead = cE('thead');
+    const trHead = cE('tr');
+    const tbody = cE('tbody');
+    table.append(thead);
+    thead.append(trHead);
+    table.append(tbody);
+    let firstLine = true;
+    let foundTerm = false;
+    data.map(function(entree){
+        /*console.log(entree);*/
+        const tr = cE('tr');
+        for(key in entree){
+            if(firstLine){
+                const th = cE('th');
+                th.append(cTN(key))
+                trHead.append(th);
+            }
+            const td = cE('td');
+            tr.append(td);
+            if(typeof entree[key] !== 'object'){
+                /*console.log(key, entree[key]);*/
+                td.append(cTN(entree[key]));
+            }else{
+                /*console.log(typeof entree[key], key);*/
+                let tdData = `<b>${key}</b><br />`;
+                for(subkey in entree[key]){
+                    if(typeof entree[key][subkey] !== 'object'){
+                        /*console.log(`---- ${subkey} ${entree[key][subkey]}`);*/
+                        tdData = tdData + `
+                            ${subkey} : <i>${entree[key][subkey]}</i><br />
+                        `;
+                    }
+                }
+                td.innerHTML = tdData;
+            }
+            /*console.log(td.innerText.toLowerCase());            */
+            if( td.innerText.toLowerCase().indexOf(searchTerm) > -1 && '' !== searchTerm){
+                foundTerm = true;
+            }
+        }
+        firstLine = false;
+        if('' === searchTerm || foundTerm){
+            tbody.append(tr);
+            foundTerm = false;
+        }else{
+            erase_childs(tr);
+            tr.remove();
+        }
+    });
+    
+    console.log(table);
+    return table;
+}
+
 loaded(function(){
     fetch('./ressources/users.json')
       .then(response => response.json())
@@ -7,7 +67,7 @@ loaded(function(){
         console.log('c\'est fini !');
     });
 
-    console.log(q('button.read'));
+    /*console.log(q('button.read'));*/
     
 
     q('button.read').map(function(button){
@@ -18,13 +78,8 @@ loaded(function(){
                 return reponse.json();
             })
             .then(function(data){
-                if(data.length > 1){
-                    data.forEach(function(user){
-                        console.log(user);
-                    });
-                }else{
-                    console.log(data);
-                }
+                erase_childs(q('#result'));
+                q('#result').append(jsonToTable(data));
             })
             .catch(function(error){
                 console.error(error);
@@ -34,5 +89,29 @@ loaded(function(){
             });
         });
     });
+
+    q('#clearResult').addEventListener('click', function(){
+        erase_childs(q('#result'));
+    });
     
+    q('button.search').addEventListener('click', function(){
+        const term = q('input[name="search"').value.toLowerCase();
+        if('' !== term){
+            fetch(this.dataset.url)
+            .then(function(reponse){
+                return reponse.json();
+            })
+            .then(function(data){
+                erase_childs(q('#result'));
+                q('#result').append(jsonToTable(data, term));
+            })
+            .catch(function(error){
+                console.error(error);
+            })
+            .finally(function(){
+                console.log('Appel terminé');
+            });
+        }
+    });
+
 });
--- js/tools.js
+++ js/tools.js
@@ -11,3 +11,22 @@ function q(selector){
         return Array.from(list)[0];
     }
 }
+
+function cE(element, options = {}){
+    const newElement = document.createElement(element);
+    return newElement;
+}
+
+function cTN(text){
+    const textNode = document.createTextNode(text);
+    return textNode;
+}
+
+function erase_childs(node){
+    if(node.childNodes){
+        let childs = node.childNodes;
+        while(childs.length > 0){
+            node.removeChild(node.lastChild);
+        }
+    }
+}

1122a1ff133e7226b11a84ca517bdc956cbf50fb
unknown <nduflot@dawan.fr>
2026-03-06 15:05:55 +0100

    TD créer un yam's en JS pt 2

--- td/Td.js
+++ td/Td.js
@@ -13,6 +13,10 @@ loaded(function(){
 
     let nbLancer = 1;
     let nbTour = 1;
+    let scoreN1 = 0
+    let scoreN2 = 0;
+    let bonus = 0;
+    let scoreTotal = 0;
     const blocked = q('#plateau [type="checkbox"]');
     const combinaisons = q('#listeCombi [type="checkbox"]');
     let combiChoisie = null;
@@ -23,6 +27,16 @@ loaded(function(){
         });
         nbLancer = 1;
         nbTour = nbTour + 1;
+    }
+
+    const desPlateau = [];
+
+    function sommePlateau(desPlateau){
+        let totalPlateau = 0;
+        desPlateau.map(function(valeur){
+            totalPlateau = totalPlateau + valeur;
+        });
+        return totalPlateau;
     }
 
     /* 
@@ -37,11 +51,13 @@ loaded(function(){
                 if(!check.checked){
                     const deRes = gobelet[cptGob].lancerDe();
                     q(check.dataset.target).innerText = deRes;
+                    desPlateau[cptGob] = deRes;
                 }
                 cptGob = cptGob + 1;
             });
             nbLancer = nbLancer + 1;            
         }
+
     });
 
     /* 
@@ -84,7 +100,66 @@ loaded(function(){
             check.checked = false;
         });
 
-        q(`#${combiChoisie}`).setAttribute('disabled', 'disabled');
+        const choixPoints = q(`#${combiChoisie}`);
+        
+        let score = 0;
+        desPlateau.sort();
+        switch(combiChoisie){
+            case 'c01':
+            case 'c02':
+            case 'c03':
+            case 'c04':
+            case 'c05':
+            case 'c06':
+                /* Éléments de la partie basse */
+                
+                desPlateau.map(function(valeur){
+                    if(choixPoints.value == valeur){
+                        score = score + valeur;
+                    }
+                });
+                scoreN1 = scoreN1 + score;
+            break;
+            case 'c07':
+            case 'c08':
+                /* brelan ou carré */
+                
+                let cpt = 1;
+                desPlateau.map(function(valeur, index, tab){
+                    if(valeur == tab[index+1]){
+                        cpt = cpt + 1;
+                        if(cpt == choixPoints.value){
+                            scoreN2 = scoreN2 + sommePlateau(desPlateau);
+                        }
+                    }else{
+                        cpt = 1;
+                    }
+
+                });
+            break;
+            case 'c09':
+                /* full */
+
+            break;
+            case 'c12':
+                let scoreYam = desPlateau[0] * 5;
+                if(scoreYam === sommePlateau(desPlateau)){
+                    scoreN2 = scoreN2 + 50;
+                }
+            break;
+            case 'c13':
+                scoreN2 = scoreN2 + sommePlateau(desPlateau);
+        }
+
+        choixPoints.setAttribute('disabled', 'disabled');
+        if(scoreN1 >= 63){
+            bonus = 35;
+        }
+        scoreTotal = scoreN1 + scoreN2 + bonus;
+        q('#scoreN1').innerText = scoreN1;
+        q('#bonus').innerText = bonus;
+        q('#scoreN2').innerText = scoreN2;
+        q('#scoreTotal').innerText = scoreTotal;
         reinit();
     });
 
